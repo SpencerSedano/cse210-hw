8ad82058810a2ced90597e9e7c6c06b787b639c3
SpencerSedano <spencer.sv20@gmail.com>
2024-04-25 13:02:46 +0800

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        /* Console.WriteLine("Hello Prep4 World!"); */
+
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter number: ");
+        string userInput = Console.ReadLine();
+        int userInputInt = int.Parse(userInput);
+
+        while (userInputInt != 0)
+        {
+            numbers.Add(userInputInt);
+
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            userInputInt = int.Parse(userInput);
+        }
+
+        int sumResult = 0;
+        foreach (int number in numbers )
+        {
+            sumResult += number;
+  
+        }
+
+        int sizeList = numbers.Count; 
+
+        Console.WriteLine($"The sum is: {sumResult}");
+        Console.WriteLine($"The average: {(float)sumResult / sizeList}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }
