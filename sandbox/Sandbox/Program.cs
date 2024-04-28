5d2aefacd888d47f6fd66c38ff6d574e79f915c3
SpencerSedano <spencer.sv20@gmail.com>
2024-04-28 11:29:46 +0800

    week2 prep learning finished

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Blind kitchen = new Blind();
+
+        kitchen._width = 60;
+        kitchen._height = 80;
+        kitchen._color = "White";
+
+        double materialAmount = kitchen.GetArea();
+
+        Console.WriteLine("Area is: " + materialAmount);
+        
     }
 }
+
+public class Blind
+{
+    public double _width;
+    public double _height;
+    public string _color;
+
+    public double GetArea()
+    {
+        return _width * _height;
+    }
+}
+
