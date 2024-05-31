ac50314eb7f2939665d5c69172210335b0c3c0c9
SpencerSedano <spencer.sv20@gmail.com>
2024-05-31 04:46:52 +0000

    starting point

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,18 @@
+class Address
+{
+    private string _street;
+    private string _city;
+    private string _StateProv;
+    private string _country;
+
+    public Address(string street, string city, string Stateprov, string country)
+    {
+        _street = street;
+        _city = city;
+        _StateProv = Stateprov;
+        _country = country;
+    }
+
+    
+
+}
